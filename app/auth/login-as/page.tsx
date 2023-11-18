b239a8bc6a0e564756562b3c76b9a9cbea6d3ff9
ADenizli <adenizlii@hotmail.com>
2023-11-18 22:43:19 +0300

    after initializing nextjs and folder structure

---
+++ app/auth/login-as/page.tsx
@@ -0,0 +1,15 @@
+import './assets/style.css';
+
+export default function LoginAsPage() {
+     return (
+          <div className='full-page-selection-container'>
+               <div className='full-page-selection-item'>
+                    <h3>As a Controller</h3>
+                    <p className='dim'>Under development</p>
+               </div>
+               <div className='full-page-selection-item'>
+                    <h3>As a Pilot</h3>
+               </div>
+          </div>
+     );
+}
